9b95b22929304951263e8e3606050220b1bddd31
André Valle <andrevvalle@icloud.com>
2014-09-09 03:01:30 -0300

    add js path

---
+++ js/main.js
@@ -0,0 +1,194 @@
+
+$(document).ready(function($) {
+
+	if ($('#slider a').length == 0) {
+		var imagem = '<a href="#" class="efect"><img src="http://dummyimage.com/800x400/000/fff.jpg&text=Andr%C3%A9+Valle+-+Nenhuma+imagem+cadastrada" alt="Imagem - Slide"></a>'
+		$('#slider').append(imagem);
+		$('.count p').text('Cadastre imagens no slide.');
+	};
+	
+	$("#form_insert").submit( function(e){
+		e.preventDefault();
+
+
+	 	if ($("#form_insert input[type=file]").val() == "") {
+	 		
+	 		$('.alert').remove();
+	 		
+	 		elemento = "<div class='alert alert-danger msg' role='alert'>Select an image!</div>"
+	 		
+	 		$('#form_insert').after(elemento);
+
+			$('.alert').fadeIn();
+			setTimeout(function() {
+		      $('.alert').fadeOut();
+			}, 2000);
+
+	 	}else{
+	 		var formData = new FormData($(this)[0]);
+	 
+			$.ajax({
+				type: 'POST',
+				dataType: 'json',
+				url: 'crud/insert.php',
+				data: formData,
+				async: false,
+				cache: false,
+				contentType: false,
+				processData: false,
+
+				success: function (data) {
+
+					$('#form_insert').wrap('<form>').trigger('reset');
+
+					var resultado = data.resultado;
+
+					$('.alert').remove();
+
+					if (data.return === 0) {
+						elemento = "<div class='alert alert-success msg' role='alert'>" + resultado + "</div>"
+					}else{
+						elemento = "<div class='alert alert-danger msg' role='alert'>" + resultado + "</div>"
+					};
+
+					$('#form_insert').after(elemento);
+
+					$('.alert').fadeIn();
+					setTimeout(function() {
+					      $('.alert').fadeOut();
+					}, 2000);
+
+				}
+
+			});
+	 	};
+	 
+	});
+
+});
+
+$('#listar').click( function(event) {
+				
+	$.ajax({
+        type: 'GET',
+        dataType: 'json',
+        url: 'crud/select.php',	
+        async: true,
+        data: "",
+        success: function(response){
+        	 
+    	 	$('#thead').next().remove();
+
+        	var tbody = $('<tbody></tbody>');
+    	 	$('#thead').after(tbody);
+
+            $.each(response, function (i, item){
+            	tr = $('#thead').next().append('<tr id="' + item.id + '"><th>' + item.id + '</th><th><img src="crud/' + item.foto + '" alt="foto ' + item.id + '" width="40" height="30"></th><th class="nome">' + item.foto + '</th><th><input type="button" id="del" value="x" onClick="funcEdit('+ item.id +')" /></th><th><input type="button" id="del" value="x" onClick="funcDel('+ item.id +')" /></th></tr>');
+            });
+        }
+    });
+
+});
+
+function funcDel(id){
+
+	var data = {id:id};
+
+	$.ajax({
+        type: 'POST',
+        dataType: 'json',
+        url: 'crud/delete.php',	
+        async: true,
+        data: data,
+        success: function(data){
+
+        	var resultado = data.resultado;
+
+			$('.alert').remove();
+			
+			var elemento = "<div class='alert alert-success msg' role='alert'>" + resultado + "</div>"
+
+			$('#listar').after(elemento);
+
+			$('.alert').fadeIn();
+			setTimeout(function() {
+			      $('.alert').fadeOut();
+			}, 2000);
+
+        	$('input').closest('#'+id).fadeOut('slow', function() {
+        		$(this).remove();
+        	});
+    	 	
+        }
+    });
+}
+
+function funcEdit(id){
+
+	$('#form_insert').hide();
+	$('#form_edit').show();
+	$('#id_image').val(id);
+
+ 	$("#form_edit").submit( function(e){
+		e.preventDefault();
+	 	
+	 	if ($("#form_edit input[type=file]").val() == "") {
+	 		
+	 		$('.alert').remove();
+	 		
+	 		elemento = "<div class='alert alert-danger msg' role='alert'>Select an image!</div>"
+	 		
+	 		$('#form_insert').after(elemento);
+
+			$('.alert').fadeIn();
+			setTimeout(function() {
+			      $('.alert').fadeOut();
+			}, 2000);
+
+	 	}else{
+
+			var formData = new FormData($(this)[0]);
+		 
+			$.ajax({
+				type: 'POST',
+				dataType: 'json',
+				url: 'crud/edit.php',
+				data: formData,
+				async: false,
+				cache: false,
+				contentType: false,
+				processData: false,
+				success: function (data) {
+
+					var resultado = data.resultado;
+
+					$('.alert').remove();
+					
+					var elemento = "<div class='alert alert-success msg' role='alert'>" + resultado + "</div>"
+
+					$('#listar').after(elemento);
+
+					$('.alert').fadeIn();
+
+					setTimeout(function() {
+					      $('.alert').hide();
+					      location.reload();
+					}, 1000);
+
+				}
+
+			});
+	 		
+	 	}
+
+	 
+	});
+
+	$("#new").click( function(e){
+		e.preventDefault();
+		$('#form_edit').hide();
+		$('#form_insert').show();
+	});
+	
+}
+
---
+++ js/slide.js
@@ -0,0 +1,87 @@
+function SliderAndre(){
+
+	var settings = {
+		primeiraImg: function(){
+			elemento = $("#slider a:first-child");
+			elemento.addClass('ativo');
+		},
+
+		slide: function(){
+			elemento = $("#slider a.ativo").next();
+
+			if (elemento.length == 0) {
+				$("#slider a").removeClass('ativo');
+				settings.primeiraImg();
+				settings.posicao();
+			}else{
+				$("#slider a").removeClass('ativo');
+				elemento.addClass('ativo');
+				var ativo = $("#slider a.ativo");
+				settings.posicao();
+			};
+
+		},
+
+		proximo: function(){
+			clearInterval(intervalo);
+			var elemento = $("#slider .ativo").next();
+
+			if (elemento.length == 0) {
+				$("#slider a").removeClass('ativo');
+				$("#slider a:first-child").addClass('ativo');
+				var ativo = $("#slider a.ativo");
+				settings.posicao();
+			}else{
+				$("#slider a").removeClass('ativo');
+				elemento.addClass('ativo');
+				var ativo = $("#slider a.ativo");
+				settings.posicao();
+			};
+
+			intervalo = setInterval(settings.slide,4000);
+		},
+
+		anterior: function(){
+			clearInterval(intervalo);
+			var elemento = $("#slider .ativo").prev();
+			
+			if (elemento.length == 0) {
+				$("#slider a").removeClass('ativo');
+				$("#slider a:last-child").addClass('ativo');
+				var ativo = $("#slider a.ativo");
+				settings.posicao();
+			}else{
+				$("#slider a").removeClass('ativo');
+				var ativo = elemento.addClass('ativo');
+				settings.posicao();
+			};
+
+			intervalo = setInterval(settings.slide,4000);
+		},
+
+		posicao: function(){
+			var pos = $('#slider a.ativo').index();
+			var posv = pos+1
+			$('.position').text(posv);
+		}
+	}
+
+	settings.primeiraImg();
+
+	var intervalo = setInterval(settings.slide,4000);
+
+	$(".set.next").click( function(){
+		settings.proximo();
+	});
+	$(".set.prev").click( function(){
+		settings.anterior();
+	});
+
+	$(document).ready( function(){
+		var total = $('#slider a').length;
+		$('.count .total').text(total);
+	});
+
+}
+
+window.addEventListener("load",SliderAndre,false);
