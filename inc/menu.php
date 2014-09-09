fd78333af38d4319dab3a7a90f9b166c695bce0e
André Valle <andrevvalle@icloud.com>
2014-09-09 03:01:14 -0300

    add path include

---
+++ inc/menu.php
@@ -0,0 +1,22 @@
+<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+  <div class="container">
+
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="index.php">GLOBO.COM</a>
+    </div>
+
+    <div class="collapse navbar-collapse" id="navbar-brand-centered">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="index.php">HOME</a></li>
+        <li><a href="system.php">SYSTEM</a></li>
+      </ul>
+    </div>
+
+  </div>
+</div>
